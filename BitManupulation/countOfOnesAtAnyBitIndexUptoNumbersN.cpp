898091e2c47dd180a1be57b5739f66f22cbe6f4b
Avinash <97391739+Avinash7390@users.noreply.github.com>
2024-06-02 23:51:52 +0530

    Update countOfOnesAtAnyBitIndexUptoNumbersN.cpp

--- BitManupulation/countOfOnesAtAnyBitIndexUptoNumbersN.cpp
+++ BitManupulation/countOfOnesAtAnyBitIndexUptoNumbersN.cpp
@@ -1,17 +1,20 @@
-vector<ll>bitcnt(64, 0);
-void cnt(ll num){
-    if(num==0)
+void cnt(ll num, vector<ll>&bitcnt){
+    if(num == 0)
     return;
+
+    ll x = leading_zeroes(num);
+
+    ll bits = 63 - x;
    
-    ll bits=1LL * log2(num);
     ll clsbit = (1LL << bits);
     bitcnt[bits] += (num - clsbit + 1);
 
-     for(int i = 0; i < bits; i++){
-         bitcnt[i] += clsbit / 2;
-     }
+    for(int i = 0; i < bits; i++){
+       bitcnt[i] += clsbit / 2;
+    }
 
-     cnt(num - clsbit);
+    cnt(num - clsbit, bitcnt);
 }
+
 
 // bitcnt[i] represents the total number of setbits of all the numbers from 1 to N at bitindex i.
