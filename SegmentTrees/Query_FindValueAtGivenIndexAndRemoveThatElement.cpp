ebe2ddcb516119276f13076232e91308a105c8a4
Avinash <97391739+Avinash7390@users.noreply.github.com>
2024-06-24 05:00:45 +0530

    Create Query_FindValueAtGivenIndexAndRemoveThatElement.cpp

---
+++ SegmentTrees/Query_FindValueAtGivenIndexAndRemoveThatElement.cpp
@@ -0,0 +1,6 @@
+// You have to first find the element at pos -> index, then remove that element. You have to do this process q times.
+//Eg. Arr: 1 2 3 4 5 6 7 8
+//qry : pos = 3
+//newArr : 1 2 4 5 6 7 8
+//qry : pos = 4
+//newArr : 1 2 4 6 7 8
